c38323c77bd91e3c7c87b81f09c6de87b4ec7c66
Yunus Emre Deligöz <ye@deligoz.me>
2025-03-13 01:31:43 +0300

    WB-1096: Add tests for Moka callback event dispatching
    Introduced tests to verify event dispatching for successful and failed Moka payment callbacks. Ensures MokaPaymentSucceeded and MokaPaymentFailed events are properly triggered based on callback response.

---
+++ tests/Feature/MokaCallbackControllerEventsTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Event;
+use Tarfin\Moka\Events\MokaPaymentFailed;
+use Tarfin\Moka\Events\MokaPaymentSucceeded;
+use Tarfin\Moka\Models\MokaPayment;
+
+beforeEach(function () {
+    Event::fake([
+        MokaPaymentSucceeded::class,
+        MokaPaymentFailed::class,
+    ]);
+});
+
+it('dispatches MokaPaymentSucceeded event when callback indicates success', function () {
+    $payment = MokaPayment::factory()->create([
+        'other_trx_code' => '12345',
+        'code_for_hash' => 'ABCDE',
+    ]);
+
+    $hashValue = hash('sha256', strtoupper('ABCDE').'T');
+
+    $this->post(route('moka-callback.handle3D'), [
+        'OtherTrxCode' => '12345',
+        'hashValue' => $hashValue,
+        'trxCode' => '67890',
+        'resultCode' => '00',
+        'resultMessage' => 'Success',
+    ]);
+
+    Event::assertDispatched(MokaPaymentSucceeded::class, static function ($event) use ($payment) {
+        return $event->mokaPayment->id === $payment->id;
+    });
+});
